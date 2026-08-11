60b9e3b59b1eb55c103613d9073ce4e9938d8d73
nodo343 <nodikogamer01@gmail.com>
2026-08-11 15:23:19 +0400

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+import importlib.util
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG and importlib.util.find_spec('debug_toolbar'):
     urlpatterns += [
         path('__debug__/', include('debug_toolbar.urls')),
     ]
