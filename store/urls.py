d083f2e0ec574e292a39ee5ae152f75ee4effca1
nodo343 <nodikogamer01@gmail.com>
2026-08-11 15:26:50 +0400

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/<slug:slug>/', views.category_products, name='category_products'),
     path('sale/', views.sale_products, name='sale_products'),
+    path('product/add/', views.product_create, name='product_create'),
     path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('product/<slug:slug>/edit/', views.product_update, name='product_update'),
+    path('product/<slug:slug>/delete/', views.product_delete, name='product_delete'),
 ]
