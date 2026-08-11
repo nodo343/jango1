32be799dae4620d7cfb4ae04de7be36315353dc9
nodo343 <nodikogamer01@gmail.com>
2026-08-11 15:33:23 +0400

    Create urls.py

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('profile/edit/', views.profile_update, name='profile_update'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+]
